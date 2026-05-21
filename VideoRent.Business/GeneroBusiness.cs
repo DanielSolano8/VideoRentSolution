88489845c7c77f93905a17f66c239ad14499f865
Daniel8 <luisdayluke@gmail.com>
2026-05-21 11:16:53 -0600

    ultimo

--- VideoRent.Business/GeneroBusiness.cs
+++ VideoRent.Business/GeneroBusiness.cs
@@ -1,7 +1,25 @@
-﻿namespace VideoRent.Business
+﻿using VideoRent.Domain;
+using VideoRent.Data;
+
+
+namespace VideoRent.Business
 {
     public class GeneroBusiness
     {
 
-    }
+        private GeneroData generoData;
+
+        public GeneroBusiness(String connectionString)
+        {
+            generoData = new GeneroData(connectionString);
+
+        }
+
+        public async Task<IEnumerable<Genero>> GetGeneros()
+        {
+            return await generoData.GetGeneros();
+
+        }
+
+        }
 }
