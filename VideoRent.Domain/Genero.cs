bf62919bfbea9782aa9499396ea43cc1de8978df
Daniel8 <luisdayluke@gmail.com>
2026-05-21 09:01:09 -0600

    aaaaaaaaaaaaaaa

---
+++ VideoRent.Domain/Genero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoRent.Domain
+{
+    public class Genero
+    {
+
+        private int generoId;
+        private String nombreGenero; 
+
+
+        public Genero() { }
+
+        public int GeneroId { get => generoId; set => generoId = value; }
+
+        public string NombreGenero { get => nombreGenero; set => nombreGenero = value; }
+
+        public Genero(int generoId, string nombreGenero)
+        {
+            this.generoId = generoId;
+            this.nombreGenero = nombreGenero;
+        }
+    }
+}
