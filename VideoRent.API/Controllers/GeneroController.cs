ed7d55455f2de2453c0c340d9eeb312157415723
Daniel8 <luisdayluke@gmail.com>
2026-05-25 09:25:28 -0600

    creacion del nuevo proyecto

---
+++ VideoRent.API/Controllers/GeneroController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideoRent.Business;
+using VideoRent.Domain;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VideoRent.API.Controllers
+{
+    [Route("api/genero")]
+    [ApiController]
+    
+        public class GeneroController : ControllerBase
+        {
+
+            private readonly IConfiguration configuration;
+
+            public GeneroController(IConfiguration configuration)
+
+            {
+
+                this.configuration = configuration;
+
+            }
+
+
+            // GET: api/<ValuesController>
+            [HttpGet]
+
+            public async Task<IEnumerable<Genero>> Get()
+
+            {
+
+                var connectionString = configuration["ConnectionStrings:VideoRentDB"];
+
+                GeneroBusiness generoBusiness = new GeneroBusiness(connectionString);
+
+                IEnumerable<Genero> geneneros = await generoBusiness.GetGeneros();
+
+                return geneneros;
+
+            }
+        }
+    }
+
+
+
+        
+    
